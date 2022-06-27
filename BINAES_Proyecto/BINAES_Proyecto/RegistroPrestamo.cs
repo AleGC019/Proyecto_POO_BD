11da905e71b8fb03b215fc3f16331fb84afbbe6c
Fernando Eguizabal <00124221@uca.edu.sv>
2022-06-26 19:10:50 -0600

    ADD: Comentarios en RegistroPrestamo

--- BINAES_Proyecto/BINAES_Proyecto/RegistroPrestamo.cs
+++ BINAES_Proyecto/BINAES_Proyecto/RegistroPrestamo.cs
@@ -12,12 +12,12 @@ namespace BINAES_Proyecto
 {
     public partial class frmRegistroPrestamo : Form
     {
-        public Usuario actualUser { get; set; }
+        // Instancias a usar dentro de programa
+        public Ejemplar eje { get; set; } //Ejemplar encontrado sujeto a prestamo
 
-        public Ejemplar eje { get; set; }
+        public List<Usuario> usuarios { get; set; } //Lista ara poblar de usuarios la combobox
 
-        public List<Usuario> usuarios { get; set; }
-
+        //Creacion de formulario
         public frmRegistroPrestamo( Ejemplar actualEjemplar)
         {
             InitializeComponent();
@@ -25,10 +25,13 @@ namespace BINAES_Proyecto
             eje = actualEjemplar;
         }
 
+        //Carga de formulario
         private void RegistroPrestamo_Load(object sender, EventArgs e)
         {
+            //Asignaciones en frio de parametros a mostrar para las fechas de prestamo
+
             DateTime hoy = DateTime.Now;
-            DateTime Limite = hoy.AddDays(14);
+            DateTime Limite = hoy.AddDays(14); //El prestamo puede hacerse un maximo de 14 dias a partir del dia en que se hace
 
             dtpEntregaFecha.Value = hoy;
             dtpEntregaFecha.Enabled = false;
@@ -46,6 +49,8 @@ namespace BINAES_Proyecto
 
             txtEjemplar.Text = eje.Nombre_Ejemplar;
 
+
+            //Poblado de combobox de usuario
             List<Usuario> poblado = new List<Usuario>();
 
             poblado = UsuarioDAO.MostrarUsuariosCMB();
@@ -58,10 +63,13 @@ namespace BINAES_Proyecto
 
             cmbUsuarioPresta.DataSource = poblado;
 
+
+            //Aviso de funcion
             MessageBox.Show("BINAES le otorgará el material por un periodo de 14 días en los que, al terminar, usted deber regresar lo prestado a mas tardar, a las 2pm.");
 
         }
 
+        //Metodo de boton cancelar registro de prestamo
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
@@ -69,12 +77,16 @@ namespace BINAES_Proyecto
             this.Close();
         }
 
+
+        //Metodo de boton de registrar prestamo
         private void btnPrestar_Click(object sender, EventArgs e)
         {
             DateTime R_entrega = dtpEntregaFecha.Value.Date + dtpEntregaHora.Value.TimeOfDay;
 
             DateTime R_devolucion = dtpDevolucionFecha.Value.Date + dtpDevolucionHora.Value.TimeOfDay;
 
+
+            //Si la funcion de la DAO devuelve true, el ingreso del registro fue exitoso.
             if (PrestamoDAO.NewPrestamo(Convert.ToInt32(cmbUsuarioPresta.SelectedValue), eje.ID, R_entrega, R_devolucion))
             {
                 MessageBox.Show("¡Ingreso exitoso!", "Exito en el registro de prestamo.", MessageBoxButtons.OK, MessageBoxIcon.None);
